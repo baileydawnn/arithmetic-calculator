be4229e7fd79b4cc0feebc7d722b36aa70092f95
Bailey Raymond <137113664+baileydawnn@users.noreply.github.com>
2023-07-12 20:05:17 -0400

    Update main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+#read from arithmetic_arranger.py file 
+from arithmetic_arranger import arithmetic_arranger
+
+#call the function, print the output
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49", "10 - 4"], False))
